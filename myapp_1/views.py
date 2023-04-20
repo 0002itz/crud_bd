692c93b2c010f4f1d939e567d6617c43c58129f7
itz <ortegagit@gmail.com>
2023-04-20 10:10:15 -0600

    hellow hord

--- myapp_1/views.py
+++ myapp_1/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from myapp_1 import views
+
 
 # Create your views here.
+def hello(request):
+    return HttpResponse("<h2>Hello word</h2>")
