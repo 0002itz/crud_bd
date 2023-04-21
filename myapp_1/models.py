ce3a3ac6d4b73aef0762ff461316d463a87948c6
itz <ortegagit@gmail.com>
2023-04-20 19:06:10 -0600

    second_model

--- myapp_1/models.py
+++ myapp_1/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class proyect (models.Model):
+class Proyect (models.Model):
     name = models.CharField(max_length=200)
+
+class Task (models.Model):
+    title = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    property = models.ForeignKey(Proyect, on_delete=models.CASCADE)
