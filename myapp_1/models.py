e092153030506e3b3f720657b587936cce18f064
itz <ortegagit@gmail.com>
2023-04-20 18:54:57 -0600

    correcion_firts model.1

--- myapp_1/models.py
+++ myapp_1/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 # Create your models here.
-class proyect (models.Model1):
-    name = models.nombre_tabla(max_length=200)
+class proyect (models.Model):
+    name = models.CharField(max_length=200)
