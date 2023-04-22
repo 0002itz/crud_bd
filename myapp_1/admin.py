f9ea5abd65b230a398273ef1fe009908ddd12810
itz <ortegagit@gmail.com>
2023-04-21 19:49:35 -0600

    modification order of models.

--- myapp_1/admin.py
+++ myapp_1/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import Proyect, Task, Question
 
 #Register your models here.
-
+admin.site.register(Question)
 admin.site.register(Proyect)
 admin.site.register(Task)
-admin.site.register(Question)
